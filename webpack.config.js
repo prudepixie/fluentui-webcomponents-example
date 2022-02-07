94316f9ebf0327bb4ef7aa60a4f2d43e3308c1e2
Wendy <wendy@Wendys-MacBook-Pro-Microsoft.local>
2022-02-07 09:50:29 -0800

    add accent button

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+<<<<<<< HEAD
 const path = require("path");
 
 module.exports = function (env, { mode }) {
@@ -6,13 +7,27 @@ module.exports = function (env, { mode }) {
   return {
     mode: production ? "production" : "development",
     devtool: production ? "source-map" : "inline-source-map",
+=======
+const path = require('path');
+
+module.exports = function (env, { mode }) {
+  const production = mode === 'production';
+  return {
+    mode: production ? 'production' : 'development',
+    devtool: production ? 'source-map' : 'inline-source-map',
+>>>>>>> c9522bf (add accent button)
     entry: {
       app: ["./src/main.ts"],
     },
     output: {
+<<<<<<< HEAD
       filename: "bundle.js",
       publicPath: "/",
       path: path.resolve(__dirname, "dist"),
+=======
+      filename: 'bundle.js',
+      publicPath: '/'
+>>>>>>> c9522bf (add accent button)
     },
     resolve: {
       extensions: [".ts", ".js"],
@@ -26,10 +41,19 @@ module.exports = function (env, { mode }) {
         writeToDisk: true,
       },
       static: {
+<<<<<<< HEAD
         directory: path.join(__dirname, "./"),
       },
     },
     plugins: [new CleanWebpackPlugin()],
+=======
+        directory: path.join(__dirname, './')
+      }
+    },
+    plugins: [
+      new CleanWebpackPlugin()
+    ],
+>>>>>>> c9522bf (add accent button)
     module: {
       rules: [
         {
