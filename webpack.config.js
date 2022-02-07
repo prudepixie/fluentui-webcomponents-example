d61dd6c25a5b92cbfbaf1574e8f73306089bdc2f
Wendy <wendy@Wendys-MacBook-Pro-Microsoft.local>
2022-02-07 09:54:23 -0800

    gitignore

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-<<<<<<< HEAD
 const path = require("path");
 
 module.exports = function (env, { mode }) {
@@ -7,27 +6,13 @@ module.exports = function (env, { mode }) {
   return {
     mode: production ? "production" : "development",
     devtool: production ? "source-map" : "inline-source-map",
-=======
-const path = require('path');
-
-module.exports = function (env, { mode }) {
-  const production = mode === 'production';
-  return {
-    mode: production ? 'production' : 'development',
-    devtool: production ? 'source-map' : 'inline-source-map',
->>>>>>> c9522bf (add accent button)
     entry: {
       app: ["./src/main.ts"],
     },
     output: {
-<<<<<<< HEAD
       filename: "bundle.js",
       publicPath: "/",
       path: path.resolve(__dirname, "dist"),
-=======
-      filename: 'bundle.js',
-      publicPath: '/'
->>>>>>> c9522bf (add accent button)
     },
     resolve: {
       extensions: [".ts", ".js"],
@@ -41,19 +26,10 @@ module.exports = function (env, { mode }) {
         writeToDisk: true,
       },
       static: {
-<<<<<<< HEAD
         directory: path.join(__dirname, "./"),
       },
     },
     plugins: [new CleanWebpackPlugin()],
-=======
-        directory: path.join(__dirname, './')
-      }
-    },
-    plugins: [
-      new CleanWebpackPlugin()
-    ],
->>>>>>> c9522bf (add accent button)
     module: {
       rules: [
         {
