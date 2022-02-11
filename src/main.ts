92016e52d134de5b280cbfb1c3e3a25f60c5c6fa
Wendy <wendy@Wendys-MacBook-Pro-Microsoft.local>
2022-02-10 17:19:15 -0800

    add card component

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,8 @@ import {
     provideFluentDesignSystem,
     fluentCard
 } from "@fluentui/web-components";
-import { ExampleCard } from "./card";
 
 provideFluentDesignSystem()
     .register(
         fluentCard()
     );
-
-ExampleCard
