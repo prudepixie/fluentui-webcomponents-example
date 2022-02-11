44259eda935e19c6af49bcd2a8dcfe9bfaa5de41
Wendy <wendy@Wendys-MacBook-Pro-Microsoft.local>
2022-02-10 17:14:52 -0800

    add card with slots

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,12 @@
 import {
     provideFluentDesignSystem,
+    fluentCard
 } from "@fluentui/web-components";
+import { ExampleCard } from "./card";
 
 provideFluentDesignSystem()
     .register(
+        fluentCard()
     );
+
+ExampleCard
