231c862385df3b42ecb7e6f1032d1ac574f52bbe
Wendy <wendy@Wendys-MacBook-Pro-Microsoft.local>
2022-03-08 16:03:06 -0800

    add query support

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,61 @@
+import { fluentButton, provideFluentDesignSystem } from "fluent-wc-v9";
 import {
-    fluentButton,
-    provideFluentDesignSystem,
-} from "fluent-wc-v9";
+  FASTElement,
+  customElement,
+  observable,
+  html,
+  when,
+  ViewTemplate,
+} from "@microsoft/fast-element";
 
-provideFluentDesignSystem()
-    .register(
-        fluentButton()
-    );
+enum Component {
+  Button = "button",
+  Mixed = "mixed",
+  Card = "card",
+}
+
+provideFluentDesignSystem().register(fluentButton());
+
+const templatesByType = {
+  button: `<fluent-button appearance="primary">Button</fluent-button>`,
+  card: `<fluent-card>I'm a Card</fluent-card>`,
+};
+const defaultTemplate = templatesByType.button;
+const template = html<ComponentList>`<div id="container">
+  ${when(
+    (x) => x.repeatNum,
+    (x) => x.componentTemplate
+  )}
+</div>`;
+
+@customElement({
+  name: "component-container",
+  template,
+})
+export class ComponentList extends FASTElement {
+  @observable repeatNum: number = 0;
+  @observable component: string | null = "";
+  templatestr: string = defaultTemplate;
+  componentTemplate: ViewTemplate = html``;
+
+  connectedCallback() {
+    super.connectedCallback();
+    const params = new URLSearchParams(document.location.search);
+    this.component = params.get("component");
+    const repeatstr = params.get("repeatNum");
+    if (repeatstr) this.repeatNum = parseInt(repeatstr);
+
+    switch (this.component) {
+      case Component.Button:
+        this.templatestr = templatesByType.button;
+        break;
+      case Component.Card:
+        this.templatestr = templatesByType.card;
+        break;
+      default:
+        this.templatestr = templatesByType.button;
+    }
+    const repeatedStr = this.templatestr.repeat(this.repeatNum);
+    this.componentTemplate = html`${repeatedStr}`;
+  }
+}
