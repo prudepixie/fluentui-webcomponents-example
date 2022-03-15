9caf1a3066cbeadf8b745d995959dfe38a84dc44
Wendy <wendy@Wendys-MacBook-Pro-Microsoft.local>
2022-03-15 10:33:58 -0700

    change from when directive to repeat

--- src/main.ts
+++ src/main.ts
@@ -1,61 +1,40 @@
-import { FluentButton, provideFluentDesignSystem } from "./fluent-wc-v9";
+import { FluentButton } from "./fluent-wc-v9";
+
+FluentButton;
 import {
   FASTElement,
   customElement,
   observable,
   html,
-  when,
-  ViewTemplate,
+  repeat,
 } from "@microsoft/fast-element";
 
-provideFluentDesignSystem().register(FluentButton);
-
-enum Component {
-  Button = "button",
-  Mixed = "mixed",
-  Card = "card",
-}
-
 const templatesByType = {
-  button: `<fluent-button appearance="primary">Button</fluent-button>`,
-  card: `<fluent-card>I'm a Card</fluent-card>`,
+  button: html`<fluent-button appearance="primary">Button</fluent-button>`,
+  card: html`<fluent-card>I'm a Card</fluent-card>`,
 };
-const defaultTemplate = templatesByType.button;
-const template = html<ComponentList>`<div id="container">
-  ${when(
-    (x) => x.repeatNum,
-    (x) => x.componentTemplate
-  )}
-</div>`;
+
+const template = html<ComponentList>`${repeat(
+  (x) => x.componentTemplate,
+  html`${(x) => x}`
+)}`;
 
 @customElement({
   name: "component-container",
   template,
 })
 export class ComponentList extends FASTElement {
-  @observable repeatNum: number = 0;
-  @observable component: string | null = "";
-  templatestr: string = defaultTemplate;
-  componentTemplate: ViewTemplate = html``;
+  @observable componentTemplate: string[] = [];
 
   connectedCallback() {
     super.connectedCallback();
     const params = new URLSearchParams(document.location.search);
-    this.component = params.get("component");
-    const repeatstr = params.get("repeatNum");
-    if (repeatstr) this.repeatNum = parseInt(repeatstr);
+    const component = params.get("component") || "button";
+    const repeatNum = parseInt(params.get("repeatNum")) || 1;
 
-    switch (this.component) {
-      case Component.Button:
-        this.templatestr = templatesByType.button;
-        break;
-      case Component.Card:
-        this.templatestr = templatesByType.card;
-        break;
-      default:
-        this.templatestr = templatesByType.button;
-    }
-    const repeatedStr = this.templatestr.repeat(this.repeatNum);
-    this.componentTemplate = html`${repeatedStr}`;
+    this.componentTemplate = Array.from(
+      { length: repeatNum },
+      (x) => templatesByType[component]
+    );
   }
 }
